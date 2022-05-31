683037533931b2cb402b9474a92e80592693eb54
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-05-31 10:04:28 +0800

    Update Surplus.py

--- Surplus.py
+++ Surplus.py
@@ -31,31 +31,46 @@ def plotdrawing_Days():
     plt.legend(["Kick", "Tend", "Deal"])
     plt.show()
 
+def dataProcess2():
+    target : pd.Series = read(tarpath)
+    features : list= [pd.DataFrame({"deal" : pd.to_datetime(target.deal_time, format = '%m/%d/%Y %H:%M:%S', errors='coerce'), "fre" : [1 for _ in range(target.deal_time.shape[0])]}), 
+                    pd.DataFrame({"kick" : pd.to_datetime(target.kick_time, format = '%m/%d/%Y %H:%M:%S', errors='coerce'), "fre" : [1 for _ in range(target.kick_time.shape[0])]}), 
+                    pd.DataFrame({"tend" : pd.to_datetime(target.tend_time, format = '%m/%d/%Y %H:%M:%S', errors='coerce'), "fre" : [1 for _ in range(target.tend_time.shape[0])]})]
+    return target.bid_price_unit_of_mkr, features
 
 def plotDraw_Months():
-    Pbid, features = dataProcess()
-    features = [pd.to_datetime(item, format="%Y-%m-%d").dt.strftime("%y-%m") for item in features]
+    Pbid, features = dataProcess2()
     #---------------for pbid----merged with kick_time----------------------------
-    Pbid = pd.concat([Pbid, features[1]], axis=1)
-    Pbid.set_index("kick_time", drop=True, inplace=True)
-    result = Pbid.groupby(level=0).sum()
-    #-----Hence we get sum of price every month(only suit for kick time) -------------------------
-    features = pd.DataFrame(features).T
-    frequency = [features[item].value_counts().sort_index() for item in features]
-    # x = frequency[0].index.values.tolist()
-    x1Index = frequency[1].index.values.tolist()
-    # x2 = frequency[2].index.values.tolist()
-    width = 0.2
-    x = [item - width for item in range(len(frequency[1]))]
-    x1 = [item for item in range(len(frequency[1]))]
-    x2 = [item + width for item in range(len(frequency[1]))]
-    frequency = [features[item].value_counts().sort_index().to_numpy().astype(int) for item in features]
-    avg = result["bid_price_unit_of_mkr"].div(frequency[1]).mul(20)
-    fig, ax = plt.subplots()
-    # fig(figsize=(10, 10))
-    ax.bar(x, frequency[0], width, color = 'tomato')
-    ax.bar(x1, frequency[1], width, color = "slateblue")
-    ax.bar(x2, frequency[2], width, color = "forestgreen")
-    ax.plot(x1Index, avg, color = "darkorange", marker = "o", linestyle = "dashed")
-    plt.legend(["20 times of Avg Monthly Price", "Deal", "Kick", "Tend"])
+    Pbid = pd.DataFrame(Pbid).set_index(features[1].kick, inplace = False)
+    features = [features[0].resample("W-Mon", on="deal").sum().reset_index().sort_values(by = "deal"), 
+                features[1].resample("W-Mon", on="kick").sum().reset_index().sort_values(by = "kick"),
+                features[2].resample("W-Mon", on="tend").sum().reset_index().sort_values(by = "tend")]
+    Pbid = Pbid.resample("W-Mon").sum().reset_index().sort_values(by="kick")
+    dates = date2num(features[1].kick)
+    ax = plt.subplot(2, 1, 1)
+    ax.bar(dates, features[0].fre, facecolor = "lightseagreen", edgecolor = "white", width = 10)
+    ax.bar(dates+10, features[1].fre, facecolor = "orangered", edgecolor = "white", width = 10)
+    ax.plot(dates, features[2].fre, color = "blueviolet", linestyle = "dashdot")
+    plt.legend(["event tend occur number", "event deal occur number", "event kick occur number"])
+    ax1 = plt.subplot(2, 1, 2)
+    ax1.plot(dates, Pbid.bid_price_unit_of_mkr, color = "tomato", linestyle = "dashed")
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_color('#DDDDDD')
+    #------hide x ticks----
+    ax.tick_params(bottom=False, left=False)
+    # draw horzonal lines from every y value
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(True, color='moccasin')
+    ax.xaxis.grid(False)
+    # -----------x axis has too many values.....
+    plt.tick_params(axis="x", which = "major", labelsize = 5)
+    ax.xaxis_date()
+    ax1.set_axisbelow(True)
+    ax1.yaxis.grid(True, color='moccasin')
+    ax1.xaxis.grid(False)
+    ax1.xaxis_date()
+    plt.title("total bid_price_unit_of_mkr")
     plt.show()
+   
